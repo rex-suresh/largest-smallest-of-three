b5eb6d08732be09d71cd9fcf640be2e1a18e6127
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-02-15 19:56:32 +0530

    Added for smallest of given three
    script can be executed via node file.js

--- high_low_of_three.js
+++ high_low_of_three.js
@@ -7,3 +7,9 @@ type = "Largest of all three"
 x = (x > y) ? x : y;
 x = (x > z) ? x : z;
 console.log(type,x);
+
+type = "Smallest of all three"
+
+x = (x < y) ? x : y;
+x = (x < z) ? x : z;
+console.log(type,x);
