8764f9ec8ee30a8e452571c372237ff9725c31e2
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-02-16 16:31:19 +0530

    Added variable names for script, removed the type variable

--- high_low_of_three.js
+++ high_low_of_three.js
@@ -1,16 +1,12 @@
 #! /usr/local/bin/node
-x = 4
-y = 3
-z = 100
+const num1 = 222
+const num2 = 3
+const num3 = 100
 
-type = "Largest of all three"
+largest_num = (num1 > num2) ? num1 : num2;
+largest_num = (largest_num > num3) ? largest_num : num3;
+console.log("Largest of all three",largest_num);
 
-x = (x > y) ? x : y;
-x = (x > z) ? x : z;
-console.log(type,x);
-
-type = "Smallest of all three"
-
-x = (x < y) ? x : y;
-x = (x < z) ? x : z;
-console.log(type,x);
+smallest_num = (num1 < num2) ? num1 : num2;
+smallest_num = (smallest_num < num3) ? smallest_num : num3;
+console.log("Smallest of all three",smallest_num);
